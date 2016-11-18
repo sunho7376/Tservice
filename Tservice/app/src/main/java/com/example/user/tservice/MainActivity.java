2684b2b364c2ee148f82f7845ad2762c7883818c
misinayu <yama_masa22@yahoo.co.jp>
2016-11-18 09:57:36 +0900

    기본 프로잭트에 이미있는 파일 추가

--- Tservice/app/src/main/java/com/example/user/tservice/MainActivity.java
+++ Tservice/app/src/main/java/com/example/user/tservice/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.tservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText edUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText edPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
--- Tservice/app/src/main/java/com/example/user/tservice/UserAreaActivity.java
+++ Tservice/app/src/main/java/com/example/user/tservice/UserAreaActivity.java
@@ -1,4 +1,4 @@
-package com.example.sunho.tservice;
+package com.example.user.tservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
