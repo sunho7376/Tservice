667c3f77e41ce7fe8f887c32244796f9bb28178a
misinayu <yama_masa22@yahoo.co.jp>
2016-12-01 23:25:55 +0900

    글 수정, 삭제 부분 추가
    근데 아직 동작이 되는지는 확인을 안했음. 그리고 화면에서 수정, 삭제 선택할 수 없음.

--- Tservice/app/src/main/java/com/example/tservice/tservice/Post.java
+++ Tservice/app/src/main/java/com/example/tservice/tservice/Post.java
@@ -9,7 +9,7 @@ import java.util.Date;
  */
 
 public class Post {
-    private int user_pk;
+    private int post_pk;
     private String title;
     private String name;
     private String place;
@@ -20,9 +20,10 @@ public class Post {
     private Date post_datetime;
     private int buyer_pk;
 
-    public Post(int user_pk, String title, String name, String place, int cost,
+    // post_pk 있는 constractor
+    public Post(int post_pk, String title, String name, String place, int cost,
                 String link, String memo, boolean sellout, Date post_datetime){
-        this.user_pk = user_pk;
+        this.post_pk = post_pk;
         this.title = title;
         this.name = name;
         this.place = place;
@@ -31,5 +32,58 @@ public class Post {
         this.memo = memo;
         this.sellout = sellout;
         this.post_datetime = post_datetime;
+    }
+
+    // post_pk 없는 constractor
+    public Post(String title, String name, String place, int cost,
+                String link, String memo, boolean sellout, Date post_datetime){
+        this.title = title;
+        this.name = name;
+        this.place = place;
+        this.cost = cost;
+        this.link = link;
+        this.memo = memo;
+        this.sellout = sellout;
+        this.post_datetime = post_datetime;
+    }
+
+    public int getPost_pk() {
+        return post_pk;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public boolean isSellout() {
+        return sellout;
+    }
+
+    public Date getPost_datetime() {
+        return post_datetime;
+    }
+
+    public int getBuyer_pk() {
+        return buyer_pk;
     }
 }
--- Tservice/app/src/main/java/com/example/tservice/tservice/RegPostActivity.java
+++ Tservice/app/src/main/java/com/example/tservice/tservice/RegPostActivity.java
@@ -1,17 +1,21 @@
 package com.example.tservice.tservice;
 
+// 글 생성, 수정, 삭제는 한 페이지로 만듬
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Date;
 
 public class RegPostActivity extends AppCompatActivity {
 
     EditText etextTitle, etextPerName, etextPerDate, etextCost, etextPerAddress, etextLink, etextMemo;
-    Button btnCreatePost;
+    Button btnCreatePost, btnRemovePost;
     Query mQuery;
 
     @Override
@@ -28,28 +32,113 @@ public class RegPostActivity extends AppCompatActivity {
         etextMemo = (EditText) findViewById(R.id.etextLink);
 
         btnCreatePost = (Button) findViewById(R.id.btnCreatePost);
-        btnCreatePost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String title = etextTitle.getText().toString();
-                String perName = etextPerName.getText().toString();
-                String perDate = etextPerDate.getText().toString();
-                String cost = etextCost.getText().toString();
-                String perAddress = etextPerAddress.getText().toString();
-                String link = etextLink.getText().toString();
-                String memo = etextMemo.getText().toString();
-                Date nowDate = new Date();
-                String dateStr = nowDate.toString();
+        btnRemovePost = (Button) findViewById(R.id.btnRemovePost);
 
-                mQuery = new Query(new CallBackListener<String>(){
-                    @Override
-                    public void onSuccess(String value) {
+        Intent intent = getIntent();
+        boolean isCreate = intent.getBooleanExtra("isCreate", true); // true값이면 세로 글 만들고 false값이면 글 수정
+        if (isCreate){
+            // 글 새로 만들기
+            btnRemovePost.setVisibility(View.INVISIBLE); // 삭제 버튼 숨기기
 
+            btnCreatePost.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String title = etextTitle.getText().toString();
+                    String perName = etextPerName.getText().toString();
+                    String perDate = etextPerDate.getText().toString();
+                    String cost = etextCost.getText().toString();
+                    String perAddress = etextPerAddress.getText().toString();
+                    String link = etextLink.getText().toString();
+                    String memo = etextMemo.getText().toString();
+                    Date nowDate = new Date();
+                    String dateStr = nowDate.toString();
+
+                    mQuery = new Query(new CallBackListener<String>(){
+                        @Override
+                        public void onSuccess(String value) {
+
+                        }
+                    }, "insert", "insert into post_open (title, name, show_datetime, place, cost, link, memo, sellout, post_datetime) " +
+                            "values (" + title + "," + perName + "," + perDate + "," + perAddress + "," + cost + "," + link + "," + memo + "," + false + "," + dateStr + ")"
+                    );
+
+                    finish();
+                }
+            });
+        } else {
+            // 글 수정, 삭제
+            btnRemovePost.setVisibility(View.VISIBLE); // 삭제 버튼 숨기기
+            final Post post = (Post) intent.getSerializableExtra("exectPost");
+
+            btnCreatePost.setText("수정");
+
+            // 존재하는 글 표시
+            Query mQuery = new Query(new CallBackListener<String>() {
+                @Override
+                public void onSuccess(String value) {
+                    String[] flag = value.split(";");
+                    if (flag[0].equals("success")) {
+                        etextTitle.setText(flag[4]);
+                        etextPerName.setText(flag[5]);
+                        etextPerDate.setText(flag[6]);
+                        etextPerAddress.setText(flag[7]);
+                        etextCost.setText(flag[8]);
+                        etextLink.setText(flag[9]);
+                        etextMemo.setText(flag[10]);
+                    } else {
+//                        //user primary key 문제시 user정보 초기화 + mainActivity로 돌려버림
+//                        User.userPk = "";
+//                        User.loginState= false;
+//                        startActivity(mainIntent);
                     }
-                }, "insert", "insert into post_open (title, name, show_datetime, place, cost, link, memo, sellout, post_datetime) " +
-                        "values (" + title + "," + perName + "," + perDate + "," + perAddress + "," + cost + "," + link + "," + memo + "," + false + "," + dateStr + ")"
-                );
-            }
-        });
+                }
+
+            }, "select", "select * from `post_open` where id='" + post.getPost_pk() + "';");
+
+            // 글 수정하는 부분
+            btnCreatePost.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String title = etextTitle.getText().toString();
+                    String perName = etextPerName.getText().toString();
+                    String perDate = etextPerDate.getText().toString();
+                    String cost = etextCost.getText().toString();
+                    String perAddress = etextPerAddress.getText().toString();
+                    String link = etextLink.getText().toString();
+                    String memo = etextMemo.getText().toString();
+                    Date nowDate = new Date();
+                    String dateStr = nowDate.toString();
+
+                    Query updateQuery = new Query(new CallBackListener<String>(){
+                        @Override
+                        public void onSuccess(String value) {
+                            Toast.makeText(RegPostActivity.this,"글이 정상적으로 수정되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }, "update", "update `post_open` set title='" + title + "', name='" + perName
+                            + "', show_datetime='" + perDate + "', place='" + perAddress
+                            + "', cost='" + cost + "', link='" + link + "', memo='" + memo
+                            + "' where id='" + post.getPost_pk() + "';"
+                    );
+
+                    finish();
+                }
+            });
+
+            // 글 삭제하는 부분
+            btnRemovePost.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Query updateQuery = new Query(new CallBackListener<String>(){
+                        @Override
+                        public void onSuccess(String value) {
+                            Toast.makeText(RegPostActivity.this,"글이 정상적으로 삭제되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }, "delete", "delete from `post_open`" + "where id='" + post.getPost_pk() + "';"
+                    );
+
+                    finish();
+                }
+            });
+        }
     }
 }
