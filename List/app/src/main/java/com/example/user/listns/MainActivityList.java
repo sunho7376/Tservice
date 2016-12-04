f0b72d5281e01a758f0262116a3aac82c74064b7
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-12-04 13:49:40 +0900

    Create ActivityList

---
+++ List/app/src/main/java/com/example/user/listns/MainActivityList.java
@@ -0,0 +1,78 @@
+package com.example.user.listns;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
+
+//글 올리기
+//branch - listActivity_by_ns
+
+public class MainActivityList extends AppCompatActivity {
+
+    EditText etextTitle, etextPerName, etextPerDate, etextCost, etextPerAddress, etextLink, etextMemo;
+    Button btnCreatePost, btnRemovePost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+
+        MyOpenHelper helper = new MyOpenHelper(this);
+        final SQLiteDatabase db = helper.getWritableDatabase();
+
+        etextTitle = (EditText) findViewById(R.id.etextTitle);
+        etextPerName = (EditText) findViewById(R.id.etextPerName);
+        etextPerDate = (EditText) findViewById(R.id.etextPerDate);
+        etextCost = (EditText) findViewById(R.id.etextCost);
+        etextPerAddress = (EditText) findViewById(R.id.etextPerAddress);
+        etextLink = (EditText) findViewById(R.id.etextLink);
+        etextMemo = (EditText) findViewById(R.id.etextLink);
+
+        btnCreatePost = (Button) findViewById(R.id.btnCreatePost);
+        btnRemovePost = (Button) findViewById(R.id.btnRemovePost);
+
+        //글 올리기
+        btnCreatePost.setOnClickListener(new btnCreatePost() {
+            //@Override
+            public void onClick(View v) {
+                String title = etextTitle.getText().toString();
+                String perName = etextPerName.getText().toString();
+                String perDate = etextPerDate.getText().toString();
+                String cost = etextCost.getText().toString();
+                String perAddress = etextPerAddress.getText().toString();
+                String link = etextLink.getText().toString();
+                String memo = etextMemo.getText().toString();
+                Date nowDate = new Date();
+                String post_datetime = nowDate.toString();
+                String sellout = "false"; //:팜매중/ true:판매완료
+
+                ContentValues insertValues = new ContentValues();
+                insertValues.put("title", title);
+                insertValues.put("perName", perName);
+                insertValues.put("perDate", perDate);
+                insertValues.put("perAddress", perAddress);
+
+                insertValues.put("cost", cost);
+                insertValues.put("link", link);
+                insertValues.put("memo", memo);
+                insertValues.put("post_datetime", post_datetime);
+                insertValues.put("sellout", title);
+                long id = db.insert("Post", name, insertValues);
+            }
+        });
+
+    }
+}
