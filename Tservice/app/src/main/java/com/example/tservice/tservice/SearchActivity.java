0cd350335844efd570bff7faafe64f79672802cf
sunho <sunho7376@naver.com>
2016-12-04 21:13:51 +0900

    feel so good

--- Tservice/app/src/main/java/com/example/tservice/tservice/SearchActivity.java
+++ Tservice/app/src/main/java/com/example/tservice/tservice/SearchActivity.java
@@ -17,56 +17,35 @@ import static java.sql.DriverManager.println;
 public class SearchActivity extends AppCompatActivity {
 
 
-    //SQLiteDatabase  db;
-    //MySQLiteOpenHelper helper; //db연동 클래스가 뭐냐 정훈아
     Button btn;
     EditText edt;
-    //String searchTitle;
+    Query mQuery;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        btn = (Button)findViewById(R.id.search_btn);
-        edt = (EditText)findViewById(R.id.edtSearch);
+        btn = (Button) findViewById(R.id.search_btn);
+        edt = (EditText) findViewById(R.id.edtSearch);
 
-        btn.setOnClickListener(new View.OnClickListener(){
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //searchTitle = edt.getText().toString();
-                //searchTable(searchTitle);
-
                 String searchWord = edt.getText().toString();
-                String[] result;
 
-                Query mQuery = new Query(new CallBackListener<String>(){
+                mQuery = new Query(new CallBackListener<String>() {
                     @Override
                     public void onSuccess(String value) {
-                        String[] arr = value.split(";");
-                        if (arr[0].equals("success")) {
-                            edt.setText(arr[1]);
-                            Log.v("LOG", "" + value);
-                        }
+                        Log.v("INSERT SEARCH::::::::", value);
                     }
-                }, "select", "select * from `post_open` where title='" + searchWord + "';");
+                }, "insert", "insert into post_open (title) " +
+                        "values ('" + searchWord + "');"
+                );
+
+                finish();
             }
         });
     }
-/**
- private void searchTable(String searchTitle){
- //database 객체를 읽기를 해주는 선언문
- db = helper.getReadableDatabase();
-
- Cursor c = db.rawQuery("Select * from " + searchTitle, null );
-
- while (c.moveToNext()){
- String title = c.getString(c.getColumnIndex("title"));
- String number = c.getString(c.getColumnIndex("number"));
- int price = c.getInt(c.getColumnIndex("price"));
-
- println(searchTitle + "제목:\n" + title + "티켓번호:\n" + number + "가격:\n" + price);
- }
- }
- **/
 }
